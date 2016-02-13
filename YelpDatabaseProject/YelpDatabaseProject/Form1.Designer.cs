6a319787e1c733f2708df21169eb13a464af294a
bgraika <bgraika@hotmail.com>
2016-02-13 14:21:48 -0800

    Have the code working for retrieving state,city,and zipcode. I dont know how to upload the Database though.

---
+++ YelpDatabaseProject/YelpDatabaseProject/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace YelpDatabaseProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.state_dropDown = new System.Windows.Forms.ComboBox();
+            this.city_listbox = new System.Windows.Forms.ListBox();
+            this.zipcode_listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // state_dropDown
+            // 
+            this.state_dropDown.FormattingEnabled = true;
+            this.state_dropDown.Location = new System.Drawing.Point(196, 99);
+            this.state_dropDown.Name = "state_dropDown";
+            this.state_dropDown.Size = new System.Drawing.Size(341, 39);
+            this.state_dropDown.TabIndex = 0;
+            this.state_dropDown.DropDown += new System.EventHandler(this.state_dropDown_DropDown);
+            this.state_dropDown.SelectedIndexChanged += new System.EventHandler(this.state_dropDown_SelectedIndexChanged);
+            // 
+            // city_listbox
+            // 
+            this.city_listbox.FormattingEnabled = true;
+            this.city_listbox.ItemHeight = 31;
+            this.city_listbox.Location = new System.Drawing.Point(196, 167);
+            this.city_listbox.Name = "city_listbox";
+            this.city_listbox.Size = new System.Drawing.Size(341, 314);
+            this.city_listbox.TabIndex = 1;
+            this.city_listbox.SelectedIndexChanged += new System.EventHandler(this.city_listbox_SelectedIndexChanged);
+            // 
+            // zipcode_listbox
+            // 
+            this.zipcode_listbox.FormattingEnabled = true;
+            this.zipcode_listbox.ItemHeight = 31;
+            this.zipcode_listbox.Location = new System.Drawing.Point(196, 514);
+            this.zipcode_listbox.Name = "zipcode_listbox";
+            this.zipcode_listbox.Size = new System.Drawing.Size(330, 221);
+            this.zipcode_listbox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1797, 1042);
+            this.Controls.Add(this.zipcode_listbox);
+            this.Controls.Add(this.city_listbox);
+            this.Controls.Add(this.state_dropDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox state_dropDown;
+        private System.Windows.Forms.ListBox city_listbox;
+        private System.Windows.Forms.ListBox zipcode_listbox;
+    }
+}
+
---
+++ YelpDatabaseProject/YelpDatabaseProject/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YelpDatabaseProject
+{
+    public partial class Form1 : Form
+    {
+        MySQL_connection mydb;
+        public Form1()
+        {
+            InitializeComponent();
+            mydb = new MySQL_connection();
+        }
+
+        //on dropdown all states in the database will appear
+        private void state_dropDown_DropDown(object sender, EventArgs e)
+        {
+            //must make a list of strings that just holds state because all
+            //we're looking for is state
+            List<string> stateColumn = new List<string>();
+            stateColumn.Add("state");
+
+            string qStr = "SELECT distinct state FROM CensusData ORDER BY state;";
+            List<List<string>> qResult = mydb.SQLSELECTExec(qStr, stateColumn);
+
+            //copy query results to the dropdown
+            for (int i = 0; i < qResult[0].Count(); i++)
+            {
+                state_dropDown.Items.Add(qResult[0][i]);
+            }
+        }
+
+        //when a state is selected, display the cities in the city listbox
+        private void state_dropDown_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //add city to the columns to look for
+            List<string> cityColumn = new List<string>();
+            cityColumn.Add("city");
+
+            string selected_state = state_dropDown.SelectedItem.ToString(); //the selected state
+            string qStr = "SELECT city FROM CensusData WHERE state= '"+ selected_state + "' ORDER BY city;";
+            List<List<string>> qResult = mydb.SQLSELECTExec(qStr, cityColumn);
+
+            //copy query results to the listbox
+            for (int i = 0; i < qResult[0].Count(); i++)
+            {
+                city_listbox.Items.Add(qResult[0][i]);
+            }
+        }
+
+        //when a city is selected display the zipcodes within that city in the zipcode listbox
+        private void city_listbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //add zipcode to the columns to look for
+            List<string> cityColumn = new List<string>();
+            cityColumn.Add("zipcode");
+
+            string selected_city = city_listbox.SelectedItem.ToString(); //the selected state
+            string qStr = "SELECT zipcode FROM CensusData WHERE city= '" + selected_city + "' ORDER BY zipcode;";
+            List<List<string>> qResult = mydb.SQLSELECTExec(qStr, cityColumn);
+
+            //copy query results to the listbox
+            for (int i = 0; i < qResult[0].Count(); i++)
+            {
+                zipcode_listbox.Items.Add(qResult[0][i]);
+            }
+        }
+
+    }
+}
